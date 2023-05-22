5f3ccc97ac49e84661eb42e782cda85f90ad0d7f
florence wangechi <florencewangechim@gmail.com>
2023-05-22 21:52:39 +0300

    Completed

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  def __init__(self):
+    self._value=None
+
+  def get_value(self):
+    return self._value
+
+  def set_value(self,new_value):
+  
+      if not isinstance(new_value,str):
+         print("The value must be a string.")
+
+      else:
+         self._value = new_value
+
+  def is_sentence(self):
+    if self.value.endswith('.'):
+            return True
+    else:
+            return False
+
+        
+  value=property(get_value,set_value)
+
